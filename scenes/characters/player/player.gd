513fa39adcc1674b1f63403cb2cf66db0a59569c
PedroSanMac <psanchezm@unsa.edu.pe>
2026-01-29 10:09:42 -0500

    agregado avance y salto a player

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -1,21 +1,27 @@
 extends CharacterBody2D
 
-@export var move_speed:float
-@export var jump_speed:float
-var facing_right = true
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var move_speed: float = 200
+@export var jump_speed: float = 400
+@export var gravity: float = 1200
 
+var facing_right := true
 
-func _physics_process(_delta: float) -> void:
-	var input_axis = Input.get_axis("left","right")
+func _physics_process(delta: float) -> void:
+	# --- Movimiento horizontal ---
+	var input_axis := Input.get_axis("left", "right")
 	velocity.x = input_axis * move_speed
-	
-	if Input.is_action_just_pressed("jump"):
+
+	# --- Flip del personaje ---
+	if input_axis != 0:
+		facing_right = input_axis > 0
+		$Sprite2D.flip_h = not facing_right
+
+	# --- Gravedad ---
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# --- Salto ---
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_speed
-	velocity.y += gravity * _delta
-	
-	if (facing_right and velocity.x<0) or (not facing_right and velocity.x>0):
-		print(velocity.x)
-		scale.x *= -1
-		facing_right = not facing_right
+
 	move_and_slide()
