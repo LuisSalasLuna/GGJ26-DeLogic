5a8eefc5efea1067b88c8e75cb17c0104cb0c906
PedroSanMac <psanchezm@unsa.edu.pe>
2026-01-29 12:34:41 -0500

    Agregado prueba de cambio de modo

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -3,13 +3,17 @@ extends CharacterBody2D
 @export var move_speed: float = 200
 @export var jump_speed: float = 400
 @export var gravity: float = 1200
+@export var speed_mode_1 := 50
+@export var speed_mode_2 := 400
 
 var facing_right := true
+var modo := 1
+var speed := speed_mode_1
 
 func _physics_process(delta: float) -> void:
 	# --- Movimiento horizontal ---
 	var input_axis := Input.get_axis("left", "right")
-	velocity.x = input_axis * move_speed
+	velocity.x = input_axis * speed
 
 	# --- Flip del personaje ---
 	if input_axis != 0:
@@ -25,3 +29,17 @@ func _physics_process(delta: float) -> void:
 		velocity.y = -jump_speed
 
 	move_and_slide()
+	
+func _input(event):
+	if event.is_action_pressed("cambiar_modo"):
+		cambiar_modo()
+		
+func cambiar_modo():
+	if modo == 1:
+		modo = 2
+		speed = speed_mode_2
+		print("Modo 2 activado")
+	else:
+		modo = 1
+		speed = speed_mode_1
+		print("Modo 1 activado")
